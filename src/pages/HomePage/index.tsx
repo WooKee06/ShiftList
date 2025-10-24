d50a1b3c787fe968d79ebb76e8b4fe84061858a1
wookee <suleimanovkamil8@gmail.com>
2025-10-24 18:21:42 +0300

    fix

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,4 +1,3 @@
-import Background from "../../widgets/background/ui/Background";
 import ShiftPlan from "../../widgets/ShiftPlan/ui/ShiftPlan";
 import TitileSection from "../../widgets/TitileSection/ui/TitileSection";
 
--- src/widgets/TitileSection/ui/TitileSection.tsx
+++ src/widgets/TitileSection/ui/TitileSection.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import s from "./TitileSection.module.scss";
 
 const TitileSection = () => {
